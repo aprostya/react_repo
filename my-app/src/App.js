8f3ce39c1eb3241919480f2a80c51cd2c0537269
a.prostyakova <a.prostyakova@rain.wtf>
2018-06-07 18:05:50 +0300

    add hide button and webpack js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,9 +8,11 @@ import {render} from "react-dom";
 function Img(props) {
     return <img src={props.src} className={props.className} alt={props.alt}/>;
 }
+
 function Buttons(props) {
     return <button {...props} />;
 }
+
 //styles
 const StButtons = styled(Buttons) `
   width: 200px;
@@ -33,38 +35,68 @@ const inline = 'inline';
 const none = 'none';
 
 class MovieTexts extends React.Component {
-    render(props) {
-        return <p {...this.props}/>
+    constructor(props) {
+        super(props);
+        this.descr = {description: "Кто мы? Какое место мы занимаем во Вселенной? Эти вопросы стоят перед героями фильма. Экипаж космического корабля С. С. Дискавери — капитаны Дэйв Боумэн, Фрэнк Пул и их бортовой компьютер ХЭЛ-9000 — должны исследовать район галактики и понять, почему инопланетяне следят за Землей. На этом пути их ждет множество неожиданных открытий…"};
     }
-}
+        render(props) {
+            return (
+                {/*<p children={this.descr.description}/>*/},
+                 <p {...this.props} children={this.descr.description}/>
+            )
+        }
+        // render(props) {
+        //     return <p {...this.props}/>
+        // }
+    }
+
+// function movieContent(props) {
+//     return (
+//         <div className="movie-content">
+//             <StButtons
+//                 className="btn-test"
+//                 type="button"
+//                 children="info"
+//                 style={{display: this.state.toggle}}
+//                 onClick={this.showText}>
+//             </StButtons>
+//         </div>
+//
+//     )
+// }
 
 
 class ToggleButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {toggle: inline};
+        this.state = {toggle: none};
         this.showText = this.showText.bind(this);
 
     }
+
     showText() {
-        const changeText = this.state.toggle == inline ? none : inline;
+        const changeText = this.state.toggle == none ? inline : none;
         this.setState({toggle: changeText});
     }
 
     render() {
         return (
-            <StButtons
-                className="btn-test"
-                type="button"
-                children="info"
-                style={{display: this.state.toggle}}
-                onClick={this.showText}>
-            </StButtons>
+            <div className="movie-content">
+                <StButtons
+                    className="btn-test"
+                    type="button"
+                    children="info"
+                    onClick={this.showText}>
+                </StButtons>
+                <MovieTexts style={{display: this.state.toggle}}/>
+            </div>
         )
     }
 }
 
-    {/*<Image src="https://i.imgur.com/82kaBeI.jpg"/>*/}
+
+{/*<Image src="https://i.imgur.com/82kaBeI.jpg"/>*/
+}
 
 
 function Movie(props) {
@@ -73,7 +105,6 @@ function Movie(props) {
             <Img className="movie-img" src={props.src} alt={props.title}/>
             <h1 className="movie-title">{props.title}</h1>
             <ToggleButton/>
-            <MovieTexts children = {"HEROINE SPORT"}/>
             <StButtonsRed className="btn-info" onClick={handleClick} id="bookBtn" type="submit" children="Click me: 0"/>
         </div>
     );
@@ -86,11 +117,13 @@ const button = document.getElementById("my-button");
 
 
 var count = 0;
+
 function handleClick() {
-        const button = document.getElementById("bookBtn");
-        count++;
-        button.innerHTML = "Click me: " + count;
-    }
+    const button = document.getElementById("bookBtn");
+    count++;
+    button.innerHTML = "Click me: " + count;
+}
+
 export default Movie;
 
 // class Movie extends React.Component {
